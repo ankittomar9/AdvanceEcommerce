d01a44b87cea495109cda0f2984eefff4fed868b
tomar <tomarankit78@gmail.com>
2025-09-10 00:27:27 +0530

    Project Setup Completed

---
+++ product-service/src/main/java/com/company/productservice/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.company.productservice.controller;
+
+import com.company.productservice.dto.ProductRequest;
+import com.company.productservice.dto.ProductResponse;
+import com.company.productservice.model.Product;
+import com.company.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+       return  productService.createProduct(productRequest);
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
+
+
+
+
+}
---
+++ product-service/src/main/java/com/company/productservice/dto/ProductRequest.java
@@ -0,0 +1,9 @@
+package com.company.productservice.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest
+        (String id, String name, String description, BigDecimal price ) {
+
+
+}
