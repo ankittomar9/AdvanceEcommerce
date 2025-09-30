59a7b7ac4df237a7d60ecfb7c2ea411ce5fe5a99
tomar <tomarankit78@gmail.com>
2025-09-30 23:10:08 +0530

    Order Service Implementation

--- order-service/src/main/java/com/company/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/company/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.company.orderservice.service;
 
+import com.company.orderservice.client.InventoryClient;
 import com.company.orderservice.dto.OrderRequest;
 import com.company.orderservice.model.Order;
 import com.company.orderservice.repository.OrderRepository;
@@ -12,18 +13,26 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository   orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        //Testing using either WireMOCK or Mockito
+        //1 using Mockito
+        //2 UseWireMock
+        var isProductInStock =inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
     //map OrderRequest to Order Object
-        Order order =new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
+        if(isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
 
-        //save order to OrderRepository
-        orderRepository.save(order);
-
+            //save order to OrderRepository
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with SkuCode " +orderRequest.skuCode()+ " is not in stock ");
+        }
 
 
     }
