c7bc4e70921af4d358cedc9928e53c535c31607a
tomar <tomarankit78@gmail.com>
2025-09-30 23:10:24 +0530

    Order Service Implementation

--- order-service/src/test/java/com/company/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/company/orderservice/OrderServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.company.orderservice;
 
-//import com.company.orderservice.stub.InventoryStubs;
+
+import com.company.orderservice.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,43 +18,43 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-//	@ServiceConnection
-//	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
-//	@LocalServerPort
-//	private Integer port;
-//
-//	@BeforeEach
-//	void setup() {
-//		RestAssured.baseURI = "http://localhost";
-//		RestAssured.port = port;
-//	}
-//
-//	static {
-//		mySQLContainer.start();
-//	}
-//
-//	@Test
-//	void shouldSubmitOrder() {
-//		String submitOrderJson = """
-//				{
-//				     "skuCode": "iphone_15",
-//				     "price": 1000,
-//				     "quantity": 1
-//				}
-//				""";
-//
-//		InventoryStubs.stubInventoryCall("iphone_15", 1);
-//		var responseBodyString = RestAssured.given()
-//				.contentType("application/json")
-//				.body(submitOrderJson)
-//				.when()
-//				.post("/api/order")
-//				.then()
-//				.log().all()
-//				.statusCode(201)
-//				.extract()
-//				.body().asString();
-//
-//		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
-//	}
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+				{
+				     "skuCode": "iphone_15",
+				     "price": 1000,
+				     "quantity": 1
+				}
+				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
 }
