e7e6ed20e86532a1e1e27ffceaa1568d5d4e974c
tomar <tomarankit78@gmail.com>
2025-09-30 23:09:21 +0530

    Inventory Service Implementation

--- order-service/src/main/java/com/company/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/com/company/orderservice/client/InventoryClient.java
@@ -1,19 +1,21 @@
 package com.company.orderservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
 //@FeignClient(value = "inventory", url = "http://localhost:8082") for localhost
 //@FeignClient(value = "inventory", url = "http://inventory-service:8082") //for docker
-@FeignClient(value = "inventory", url = "${inventory.service.url}") //for docker
+//@FeignClient(value = "inventory", url = "${inventory.service.url}") //for docker
 
 
 public interface InventoryClient {
 
 //    @GetMapping
 //    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
-        @RequestMapping(method = RequestMethod.GET,value = "/api/inventory")
+       // @RequestMapping(method = RequestMethod.GET,value = "/api/inventory")
+        @GetExchange("/api/inventory")
         boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
